fb86d95482801c7ac14acfe8fd99925f4f49e369
Julián Melero Hidalgo <dev@julianmelero.dev>
2021-06-10 17:09:43 +0200

    Swagger

--- api/index.js
+++ api/index.js
@@ -2,13 +2,17 @@ const express = require("express");
 const config = require("../config.js");
 const user = require("./components/user/network");
 
+const swaggerUi = require("swagger-ui-express");
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+const swaggerDoc = require("./swagger.json");
+
 // ROUTING
 app.use("/api/user", user);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 
 // Enable Server
 app.listen(config.api.port, () => {
